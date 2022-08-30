99383abf4c25946bf79db2612e7f0834e2371a07
Zetian612o <jsperdomo361@misena.edu.co>
2022-08-29 19:48:19 -0500

    Agregar configuracion de Heroku

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use((req, res) => {
   res.status(404).json({ error: '404 Not found' })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
